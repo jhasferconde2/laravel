2d60ab6736b671b87c77d851bb07ddd85e8ec628
unknown <cit183g-36@M303.UC-BCF.EDU.PH>
2023-02-08 16:12:38 +0800

    asdad

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,6 +10,10 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    public function chirps()
+    {
+        return $this->hasMany(Chirp::class);
+    }
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
